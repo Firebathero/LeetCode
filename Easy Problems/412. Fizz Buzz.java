a15769407e128a33af6de6049a35991b03c882e7
Ryan Furton <ryanfurton@gmail.com>
2021-01-15 07:47:37 -0500

    Update 412. Fizz Buzz.java

--- Easy Problems/412. Fizz Buzz.java
+++ Easy Problems/412. Fizz Buzz.java
@@ -1,7 +1,7 @@
 class Solution {
   public List < String > fizzBuzz(int n) {
     ArrayList < String > ans = new ArrayList < >();
-    for (int i = 1; i < n + 1; i++) {
+    for (int i = 1; i <= n; i++) {
       if (i % 3 == 0 && i % 5 != 0) {
         ans.add("Fizz");
       }
