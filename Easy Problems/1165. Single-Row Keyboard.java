56c8dad081bc0f52afa8d637014e44627054c4c9
Ryan Furton <ryanfurton@gmail.com>
2021-01-15 06:09:27 -0500

    Update 1165. Single-Row Keyboard.java

--- Easy Problems/1165. Single-Row Keyboard.java
+++ Easy Problems/1165. Single-Row Keyboard.java
@@ -1,26 +1,21 @@
+//First, fill hashmap with custom keyboard inputs and their index (which will be used to track distance between them)
+
 class Solution {
-    
-    public int calculateTime(String keyboard, String word) {
-        
-        int index = 0;
-        HashMap<Character, Integer> layout = new HashMap<>();
-        
-        //populate alphabet map with [char,value]
-        for(char c: keyboard.toCharArray()){
-            layout.put(c, index);
-            index++;
-        }
-        
-        /*
-        for each char in word[], get value at char in map
-        compare value at c to previous
-        */
-        int steps = 0, prev = 0;
-        for(char c: word.toCharArray()){
-            int temp = layout.get(c);
-            steps += Math.abs(prev - temp);
-            prev = temp;
-        }
-        return steps;
+  public int calculateTime(String keyboard, String word) {
+    HashMap <Character, Integer> keyMap = new HashMap<>();
+    for (int i = 0; i < keyboard.length(); i++) {
+      keyMap.put(keyboard.charAt(i), i);
     }
+      
+//Calculate the absolute diff between starting position (prevKey) and the current position, then set current to prev and repeat for all chars in word
+      
+    int time = 0;
+    int prevKey = 0;   
+    for (int j = 0; j < word.length(); j++) {
+      int currKey = keyMap.get(word.charAt(j));
+      time += Math.abs(currKey - prevKey);
+      prevKey = currKey;
+    }
+    return time;
+  }
 }
